e1f50fb1a5765afb1b3d6d154cc6f67e9c2ed316
NiccoloPanichi <73541729+NiccoloPanichi@users.noreply.github.com>
2020-12-20 17:25:37 +0100

    ProductsSplit

--- src/main/java/DataChanges.java
+++ src/main/java/DataChanges.java
@@ -2,54 +2,69 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class DataChanges {
-    public void go(){
-    JSONParser parser = new JSONParser();
+
+    private JSONArray rawProducts;
+
+    public void parseFile() {
+
+        File productsFile = new File("products.json");
+
+        if (productsFile.exists()) {
+            productsFile.delete();
+        }
+
         try {
-        Object obj = parser.parse(new FileReader("C:/Users/Giovanni/Desktop/aaaaa.json"));
-        JSONArray employeeList = (JSONArray) obj;
-        JSONObject newObj=new JSONObject();
-        employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp, newObj, newObj) );
-    } catch (Exception e) {
-        e.printStackTrace();
+
+            final FileWriter fw = new FileWriter(productsFile);
+
+            JSONParser parser = new JSONParser();
+
+            Object rawData = parser.parse(new FileReader("row-data-Amazon.json"));
+            rawProducts = (JSONArray) rawData;
+
+            rawProducts.forEach(rawProd -> parseProductObject((JSONObject) rawProd, fw));
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
-    private static void parseEmployeeObject(JSONObject employee,JSONObject newJson, JSONObject newReview)
-    {
-        //products
 
-        String id = (String) employee.get("id");
-        String name = (String) employee.get("name");
-        String brand = (String) employee.get("brand");
+    private static void parseProductObject(JSONObject rawProd, FileWriter fw){
 
+        //retrive the details of the product
+        String id = (String) rawProd.get("id");
+        String name = (String) rawProd.get("name");
+        String brand = (String) rawProd.get("brand");
+        String[] categories = ((String) rawProd.get("categories")).split(",");
+        String mainCategory = (String) rawProd.get("primaryCategories");
 
-        String[] categories = ((String) employee.get("categories")).split(",");
+        //create a cleared product
+        JSONObject product = new JSONObject();
+        product.put("id", id);
+        product.put("name", name);
+        product.put("brand", brand);
+
         JSONArray array = new JSONArray();
-        for(int i = 0; i < categories.length; i++) {
+        for (int i = 0; i < categories.length; i++) {
             array.add(categories[i]);
         }
 
-        String primaryCategories = (String) employee.get("primaryCategories");
-        Long reviews_id = (Long) employee.get("reviews.id");
+        product.put("categories", array);
+        product.put("mainCategory", mainCategory);
 
-
-
-        newJson.put("id",id);
-        newJson.put("name",name);
-        newJson.put("brand",brand);
-        newJson.put("categories",array);
-        newJson.put("primaryCategories",primaryCategories);
-        newJson.put("idReview",reviews_id);
-
-
-
-
-
-
-
-
+        try {
+            fw.append(product.toJSONString() + "," + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+/*
         //reviews
         String reviewDate = (String) employee.get("reviews.date");
         Boolean reviewDoRaccomend = (Boolean) employee.get("reviews.doRecommend");
@@ -59,16 +74,14 @@ public class DataChanges {
         String reviewUsername = (String) employee.get("reviews.Username");
 
 
+        newReview.put("id", reviews_id);
+        newReview.put("date", reviewDate);
+        newReview.put("doRaccomend", reviewDoRaccomend);
+        newReview.put("rating", reviewRating);
+        newReview.put("text", reviewText);
+        newReview.put("title", reviewTitle);
+        newReview.put("username", reviewUsername);
 
 
-        newReview.put("id",reviews_id);
-        newReview.put("date",reviewDate);
-        newReview.put("doRaccomend",reviewDoRaccomend);
-        newReview.put("rating",reviewRating);
-        newReview.put("text",reviewText);
-        newReview.put("title",reviewTitle);
-        newReview.put("username",reviewUsername);
-
-
-    }
+    }*/
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,6 @@ public class Test {
     public static void main(String[] args){
 
         DataChanges dg = new DataChanges();
-        dg.go();
+        dg.parseFile();
     }
 }
