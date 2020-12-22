4c578e40088a3dd6c788317aae2b264bad4f8abd
NiccoloPanichi <73541729+NiccoloPanichi@users.noreply.github.com>
2020-12-22 09:56:38 +0100

    RandomPrice

--- src/main/java/utilities/RandomGen.java
+++ src/main/java/utilities/RandomGen.java
@@ -1,8 +1,19 @@
 package utilities;
 
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class RandomGen {
+
     public static String generateRandomPassword(int length) {
         // Possible characters inside the password (0-9, a-z, A-Z)
         final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -17,5 +28,17 @@ public class RandomGen {
         }
 
         return stringBuilder.toString();
+    }
+
+    public static double generateRandomPrice() {
+
+        double min = 90;
+        double max = 650;
+        double random = new Random().nextDouble();
+        double price = min + (random * (max - min));
+
+        price = Math.round(price * 100.0) / 100.0;
+
+        return price;
     }
 }
